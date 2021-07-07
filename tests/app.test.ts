61f5fad69378e91796d0bf3aad06fc02e78f0fc3
Tyler C. Yates <tyates907@gmail.com>
2021-07-06 17:12:17 -0700

    start post user tests

---
+++ tests/app.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import app from '../src/app'
+
+
+
+describe('User Routes', () => {
+
+  describe('POST /user', () => {
+    it('should return a 201 status', async () => {
+        const response = await request(app).post("/user").send({})
+        expect(response.status).toBe(201);
+    });
+      it('should be of content type json', async () => {
+          const response = await request(app).post("/user").send({})
+          expect(response.headers['content-type']).toEqual(expect.stringContaining("json"));
+      });
+  })
+  
+
+  
+
+})
+
